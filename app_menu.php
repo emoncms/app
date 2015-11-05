ae3af75ee793eb98e9bd2d6b8a40c74aaf9478f4
Trystan Lea <trystan.lea@googlemail.com>
2015-11-05 17:18:32 +0000

    Update app_menu.php

--- app_menu.php
+++ app_menu.php
@@ -11,15 +11,10 @@
         'path'=>"app/mysolarpv" , 
         'session'=>"write", 
         'order' => 5,
-        'icon'=>'icon-leaf icon-white',
         'dropdown'=>array(
-            
-            array('name' => 'My Electric', 'icon' => '', 'path' => "app$apikey#myelectric", 'session' => 'read', 'order' => 1),
-            array('name' => 'My Solar', 'icon' => '', 'path' => "app$apikey#mysolarpv", 'session' => 'read', 'order' => 2),
-            array('name' => 'My Heatpump', 'icon' => '', 'path' => "app$apikey#myheatpump", 'session' => 'read', 'order' => 3),
-            array('name' => 'My Solar&amp;Wind', 'icon' => '','path' => "app$apikey#myenergy", 'session' => 'read', 'order' => 4)
-            
+            array("My Electric","app$apikey#myelectric"),
+            array("My Solar","app$apikey#mysolarpv"),
+            array("My Heatpump","app$apikey#myheatpump"),
+            array("My Solar&Wind","app$apikey#myenergy")
         )
     );
-    
-    
