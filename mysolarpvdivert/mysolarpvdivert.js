54948f17755c1d198c1674e885655e9dea909056
Matt Galloway <matt@galloway.me.uk>
2016-08-28 10:10:29 +0100

    Fix labels on the history bar graph page

--- mysolarpvdivert/mysolarpvdivert.js
+++ mysolarpvdivert/mysolarpvdivert.js
@@ -575,8 +575,8 @@ var app_mysolarpvdivert = {
         
         var plot = $.plot($('#mysolarpvdivert_placeholder'),app_mysolarpvdivert.historyseries,options);
         
-        $('#mysolarpvdivert_placeholder').append("<div style='position:absolute;left:50px;top:30px;color:#666;font-size:12px'><b>Above:</b> Onsite Use & Total Use</div>");
-        $('#mysolarpvdivert_placeholder').append("<div style='position:absolute;left:50px;bottom:50px;color:#666;font-size:12px'><b>Below:</b> Exported solar</div>");
+        $('#mysolarpvdivert_placeholder').append("<div style='position:absolute;left:50px;top:30px;color:#666;font-size:12px'><b>Above:</b> House used, diverted & exported solar</div>");
+        $('#mysolarpvdivert_placeholder').append("<div style='position:absolute;left:50px;bottom:50px;color:#666;font-size:12px'><b>Below:</b> Total usage</div>");
 
         // Because the bargraph is only drawn once when the view is changed we attach the events at this point
         app_mysolarpvdivert.bargraph_events();
