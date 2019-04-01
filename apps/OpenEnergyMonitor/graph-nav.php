e35d7d2661dd894b6e802202d21f98c158f8d036
Emrys <emrys@openenergymonitor.org>
2019-04-01 14:12:17 +0100

    merged mobile resize edits

--- apps/OpenEnergyMonitor/graph-nav.php
+++ apps/OpenEnergyMonitor/graph-nav.php
@@ -1,9 +1,9 @@
 <div class="visnavblock mb-2 d-flex justify-content-start d-flex justify-content-stretch btn-group">
-    <button class='visnav myelectric-time btn btn-inverse btn-link btn-large py-1 px-2' time='3'>3<?php echo _('h') ?></button>
-    <button class='visnav myelectric-time btn btn-inverse btn-link btn-large py-1 px-2' time='6'>6<?php echo _('h') ?></button>
-    <button class='visnav myelectric-time btn btn-inverse btn-link btn-large py-1 px-2' time='24'><?php echo _('D') ?></button>
-    <button class='visnav myelectric-time btn btn-inverse btn-link btn-large py-1 px-2' time='168'><?php echo _('W') ?></button>
-    <button class='visnav myelectric-time btn btn-inverse btn-link btn-large py-1 px-2' time='720'><?php echo _('M') ?></button>
+    <button class='visnav time btn btn-inverse btn-link btn-large py-1 px-2' time='3'>3<?php echo _('h') ?></button>
+    <button class='visnav time btn btn-inverse btn-link btn-large py-1 px-2' time='6'>6<?php echo _('h') ?></button>
+    <button class='visnav time btn btn-inverse btn-link btn-large py-1 px-2' time='24'><?php echo _('D') ?></button>
+    <button class='visnav time btn btn-inverse btn-link btn-large py-1 px-2' time='168'><?php echo _('W') ?></button>
+    <button class='visnav time btn btn-inverse btn-link btn-large py-1 px-2' time='720'><?php echo _('M') ?></button>
     <button id='zoomin' class='visnav btn btn-inverse btn-link btn-large py-1 px-2' >+</button>
     <button id='zoomout' class='visnav btn btn-inverse btn-link btn-large py-1 px-2' >-</button>
     <button id='left' class='visnav btn btn-inverse btn-link btn-large py-1 px-2' >&lt;</button>
