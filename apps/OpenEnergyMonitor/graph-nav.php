620f80bd7d827326f72c4e89dc14eee23cbec426
Emrys <emrys@openenergymonitor.org>
2019-04-02 13:50:27 +0100

    added year button to graph navigation

--- apps/OpenEnergyMonitor/graph-nav.php
+++ apps/OpenEnergyMonitor/graph-nav.php
@@ -4,6 +4,7 @@
     <button class='visnav time btn btn-inverse btn-link btn-large py-1 px-2' time='24'><?php echo _('D') ?></button>
     <button class='visnav time btn btn-inverse btn-link btn-large py-1 px-2' time='168'><?php echo _('W') ?></button>
     <button class='visnav time btn btn-inverse btn-link btn-large py-1 px-2' time='720'><?php echo _('M') ?></button>
+    <button class='visnav time btn btn-inverse btn-link btn-large py-1 px-2' time='8760'><?php echo _('Y') ?></button>
     <button id='zoomin' class='visnav btn btn-inverse btn-link btn-large py-1 px-2' >+</button>
     <button id='zoomout' class='visnav btn btn-inverse btn-link btn-large py-1 px-2' >-</button>
     <button id='left' class='visnav btn btn-inverse btn-link btn-large py-1 px-2' >&lt;</button>
