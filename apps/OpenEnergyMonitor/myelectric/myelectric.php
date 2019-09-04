355fc9805b28b2ad00af7d95378e52907da2895c
Emrys <emrys@openenergymonitor.org>
2019-09-04 13:09:05 +0100

    used new emoncms 'resized' event in all apps removed 'resize' event all all apps stopped repeat calling of data by using new events https://github.com/emoncms/emoncms/pull/1397

--- apps/OpenEnergyMonitor/myelectric/myelectric.php
+++ apps/OpenEnergyMonitor/myelectric/myelectric.php
@@ -297,25 +297,6 @@ function resize()
     $("#placeholder_power").attr('height',height); 
     graph_lines.height = height;
     
-    // if (width<=500) {
-    //     $(".electric-title").css("font-size","16px");
-    //     $(".power-value").css("font-size","38px");
-    //     $(".units").hide();
-    //     $(".visnav").css("padding-left","5px");
-    //     $(".visnav").css("padding-right","5px");
-    // } else if (width<=724) {
-    //     $(".electric-title").css("font-size","18px");
-    //     $(".power-value").css("font-size","52px");
-    //     $(".units").show();
-    //     $(".visnav").css("padding-left","8px");
-    //     $(".visnav").css("padding-right","8px");
-    // } else {
-    //     $(".electric-title").css("font-size","22px");
-    //     $(".power-value").css("font-size","85px");
-    //     $(".units").show();
-    //     $(".visnav").css("padding-left","8px");
-    //     $(".visnav").css("padding-right","8px");
-    // }
     if($('#app-block').is(":visible")) {
         reloadkwhd = true;
         fastupdate();
@@ -608,14 +589,9 @@ function slowupdate()
     graph_bars.draw('placeholder_kwhd',[daily]);
     $(".ajax-loader").hide();
 }
-
-var resizeTimer;
-window.addEventListener("resize", function() {
-    // debounce (ish) script to improve performance
-    clearTimeout(resizeTimer);
-    resizeTimer = setTimeout(function() {
-        resize()
-    }, 400)
+// on finish sidebar hide/show
+$(function() {
+    $(document).on('window.resized hidden.sidebar.collapse shown.sidebar.collapse', resize)
 })
 // ----------------------------------------------------------------------
 // App log
