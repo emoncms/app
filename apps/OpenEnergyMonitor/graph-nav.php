f3bbb297b65b5a84cabf4e32cfd9074ce7807e91
TrystanLea <trystan.lea@gmail.com>
2020-04-08 08:29:08 +0000

    fix height of mysolarpv app

--- apps/OpenEnergyMonitor/graph-nav.php
+++ apps/OpenEnergyMonitor/graph-nav.php
@@ -1,4 +1,5 @@
 <div id="graph-nav" class="visnavblock mb-2 d-flex justify-content-start d-flex justify-content-stretch btn-group">
+    <button class='visnav time btn btn-inverse btn-link btn-large py-1 px-2' time='1'>1<?php echo _('h') ?></button>
     <button class='visnav time btn btn-inverse btn-link btn-large py-1 px-2' time='3'>3<?php echo _('h') ?></button>
     <button class='visnav time btn btn-inverse btn-link btn-large py-1 px-2' time='6'>6<?php echo _('h') ?></button>
     <button class='visnav time btn btn-inverse btn-link btn-large py-1 px-2' time='24'><?php echo _('D') ?></button>
