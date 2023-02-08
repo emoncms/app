4f75a9e1f43d7458e329e2cc16a1a216a8400f3e
TrystanLea <trystan.lea@gmail.com>
2023-02-08 16:20:29 +0000

    update apii

--- apps/OpenEnergyMonitor/myheatpump/myheatpump_api.php
+++ apps/OpenEnergyMonitor/myheatpump/myheatpump_api.php
@@ -25,19 +25,27 @@ function get_heatpump_stats($feed,$app,$start,$end,$starting_power) {
 
 
 
-    if (!isset($app->config->heatpump_flowT) || $app->config->heatpump_flowT<1) return false;
-    if (!isset($app->config->heatpump_returnT) || $app->config->heatpump_returnT<1) return false;
-    if (!isset($app->config->heatpump_outsideT) || $app->config->heatpump_outsideT<1) return false;
-    if (!isset($app->config->heatpump_elec) || $app->config->heatpump_elec<1) return false;
-    if (!isset($app->config->heatpump_heat) || $app->config->heatpump_heat<1) return false;
-    
+    if (!isset($app->config->heatpump_flowT) || $app->config->heatpump_flowT<1) return array('success'=>false, 'message'=>"Missing flow temperature feed");
+    if (!isset($app->config->heatpump_elec) || $app->config->heatpump_elec<1) return array('success'=>false, 'message'=>"Missing electricity consumption feed");
+        
     $elec_meta = $feed->get_meta($app->config->heatpump_elec);
-    
-    $flowT_data = $feed->get_data($app->config->heatpump_flowT,$start,$end,$interval,1,"UTC","notime",false,0,0,false);
-    $returnT_data = $feed->get_data($app->config->heatpump_returnT,$start,$end,$interval,1,"UTC","notime",false,0,0,false);
-    $outsideT_data = $feed->get_data($app->config->heatpump_outsideT,$start,$end,$interval,1,"UTC","notime",false,0,0,false);
     $elec_data = $feed->get_data($app->config->heatpump_elec,$start,$end,$interval,1,"UTC","notime",false,0,0,false);
-    $heat_data = $feed->get_data($app->config->heatpump_heat,$start,$end,$interval,1,"UTC","notime",false,0,0,false);
+    $flowT_data = $feed->get_data($app->config->heatpump_flowT,$start,$end,$interval,1,"UTC","notime",false,0,0,false);
+
+    $returnT_data = false;
+    if (isset($app->config->heatpump_returnT) && $app->config->heatpump_returnT>0) {
+        $returnT_data = $feed->get_data($app->config->heatpump_returnT,$start,$end,$interval,1,"UTC","notime",false,0,0,false);
+    }
+  
+    $outsideT_data = false;
+    if (isset($app->config->heatpump_outsideT) && $app->config->heatpump_outsideT>0) {
+        $outsideT_data = $feed->get_data($app->config->heatpump_outsideT,$start,$end,$interval,1,"UTC","notime",false,0,0,false);
+    }
+    
+    $heat_data = false;
+    if (isset($app->config->heatpump_heat) && $app->config->heatpump_heat>0) {
+        $heat_data = $feed->get_data($app->config->heatpump_heat,$start,$end,$interval,1,"UTC","notime",false,0,0,false);
+    }
     
     $condensing_offset = 4;
     $evaporator_offset = -6;
@@ -72,10 +80,10 @@ function get_heatpump_stats($feed,$app,$start,$end,$starting_power) {
         if ($time<$elec_meta->end_time) {
             
             if (!is_null($elec_data[$z])) $elec = $elec_data[$z];
-            if (!is_null($heat_data[$z])) $heat = $heat_data[$z];        
+            if ($heat_data && !is_null($heat_data[$z])) $heat = $heat_data[$z];        
             if (!is_null($flowT_data[$z])) $flowT = $flowT_data[$z];
-            if (!is_null($returnT_data[$z])) $returnT = $returnT_data[$z];
-            if (!is_null($outsideT_data[$z])) $ambientT = $outsideT_data[$z];
+            if ($returnT_data && !is_null($returnT_data[$z])) $returnT = $returnT_data[$z];
+            if ($outsideT_data && !is_null($outsideT_data[$z])) $ambientT = $outsideT_data[$z];
             
             $elec_kwh += $elec * $interval / 3600000.0;
             $heat_kwh += $heat * $interval / 3600000.0;           
@@ -110,10 +118,11 @@ function get_heatpump_stats($feed,$app,$start,$end,$starting_power) {
     $ideal_carnot_heat_mean = $ideal_carnot_heat_sum / $running_count;
     $prc_of_carnot = (100 * $heat_mean / $ideal_carnot_heat_mean);
     
-    return [
-      "start"=>$start,
-      "end"=>$end,
-      "interval"=>$interval,
+
+    $result = [
+      "start"=>(int)$start,
+      "end"=>(int)$end,
+      "interval"=>(int)$interval,
       "datapoints"=>count($elec_data),
       "full_period"=>[
         "elec_kwh"=>number_format($elec_kwh,3,'.','')*1,
@@ -136,6 +145,27 @@ function get_heatpump_stats($feed,$app,$start,$end,$starting_power) {
         "carnot_prc"=>number_format($prc_of_carnot,2,'.','')*1
       ]
     ];
+
+    if (!$heat_data) {
+      $result["full_period"]["heat_kwh"] = false;
+      $result["when_running"]["heat_kwh"] = false;
+      $result["when_running"]["heat_W"] = false;
+      $result["when_running"]["carnot_prc"] = false;
+    }
+    
+    if (!$outsideT_data) {
+      $result["when_running"]["outsideT"] = false;
+      $result["when_running"]["flow_minus_outside"] = false;
+      $result["when_running"]["carnot_prc"] = false;
+    }
+
+    if (!$returnT_data) {
+      $result["when_running"]["returnT"] = false;
+      $result["when_running"]["flow_minus_return"] = false;
+      $result["when_running"]["carnot_prc"] = false;
+    }
+    
+    return $result;
 }
 
 function convert_time($time,$timezone) {
