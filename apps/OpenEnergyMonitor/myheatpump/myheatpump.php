16148c2934c92328c675ca185d8643f8b51f1280
TrystanLea <trystan.lea@gmail.com>
2023-11-09 10:56:08 +0000

    update

--- apps/OpenEnergyMonitor/myheatpump/myheatpump.php
+++ apps/OpenEnergyMonitor/myheatpump/myheatpump.php
@@ -245,5 +245,5 @@ var session_write = <?php echo $session['write']; ?>;
 config.name = "<?php echo $name; ?>";
 config.db = <?php echo json_encode($config); ?>;
 </script>
-<script type="text/javascript" src="<?php echo $path; ?>Modules/app/apps/OpenEnergyMonitor/myheatpump/myheatpump.js?v=59"></script>
+<script type="text/javascript" src="<?php echo $path; ?>Modules/app/apps/OpenEnergyMonitor/myheatpump/myheatpump.js?v=60"></script>
 
