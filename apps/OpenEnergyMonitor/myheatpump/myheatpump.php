ea54b2f0d36ecc4d0268f953b2b286118db2f52b
TrystanLea <trystan.lea@gmail.com>
2023-12-09 22:30:12 +0000

    remove engine requirement

--- apps/OpenEnergyMonitor/myheatpump/myheatpump.php
+++ apps/OpenEnergyMonitor/myheatpump/myheatpump.php
@@ -321,4 +321,4 @@ var session_write = <?php echo $session['write']; ?>;
 config.name = "<?php echo $name; ?>";
 config.db = <?php echo json_encode($config); ?>;
 </script>
-<script type="text/javascript" src="<?php echo $path; ?>Modules/app/apps/OpenEnergyMonitor/myheatpump/myheatpump.js?v=85"></script>
+<script type="text/javascript" src="<?php echo $path; ?>Modules/app/apps/OpenEnergyMonitor/myheatpump/myheatpump.js?v=86"></script>
