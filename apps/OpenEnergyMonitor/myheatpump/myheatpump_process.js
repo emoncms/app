3686847624276f4bd3f9f4424b5ee359216250f4
Trystan Lea <trystan.lea@gmail.com>
2024-09-28 09:11:29 +0100

    check if kwh undefined

--- apps/OpenEnergyMonitor/myheatpump/myheatpump_process.js
+++ apps/OpenEnergyMonitor/myheatpump/myheatpump_process.js
@@ -320,7 +320,10 @@ function process_stats() {
     }
     
     // Standby energy
-    var standby_kwh = stats['combined']['heatpump_elec'].kwh - stats['when_running']['heatpump_elec'].kwh;
+    var standby_kwh = 0;
+    if (stats['combined']['heatpump_elec'].kwh != undefined && stats['when_running']['heatpump_elec'].kwh != undefined) {
+        standby_kwh = stats['combined']['heatpump_elec'].kwh - stats['when_running']['heatpump_elec'].kwh;
+    }
     $("#standby_kwh").html(standby_kwh.toFixed(3));
 
     return stats;
