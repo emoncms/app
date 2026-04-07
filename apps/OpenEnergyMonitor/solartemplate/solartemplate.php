2df154138fcfd26c830a4bf3ed4ef1f3c7c9d645
Trystan Lea <trystan.lea@gmail.com>
2026-04-07 12:36:15 +0100

    update

--- apps/OpenEnergyMonitor/solartemplate/solartemplate.php
+++ apps/OpenEnergyMonitor/solartemplate/solartemplate.php
@@ -3,9 +3,6 @@ defined('EMONCMS_EXEC') or die('Restricted access');
 global $path, $session, $v;
 ?>
 <link href="<?php echo $path; ?>Modules/app/Views/css/dark.css?v=<?php echo $v; ?>" rel="stylesheet">
-
-<link href="<?php echo $path; ?>Modules/app/Lib/appconf/appconf.css?v=<?php echo $v; ?>" rel="stylesheet">
-<script type="text/javascript" src="<?php echo $path; ?>Modules/app/Lib/appconf/appconf.js?v=<?php echo $v; ?>"></script>
 <script type="text/javascript" src="<?php echo $path; ?>Modules/feed/feed.js?v=<?php echo $v; ?>"></script>
 
 <script type="text/javascript" src="<?php echo $path; ?>Lib/flot/jquery.flot.min.js?v=<?php echo $v; ?>"></script>
@@ -138,20 +135,7 @@ global $path, $session, $v;
     
 </div>
 
-<!-- App configuration -->
-<section id="app-setup" class="hide pb-3">
-    <div class="px-3">
-        <div class="row-fluid">
-            <div class="span7 xapp-config-description">
-                <div class="xapp-config-description-inner text-light">
-                    <h2 class="app-config-title text-primary"><?php echo tr('Solar Template App'); ?></h2>
-                    <p class="lead">A simpler version of the My Solar Battery app, power view only and key calculations</p>
-                </div>
-            </div>
-            <div class="span5 app-config pt-3"></div>
-        </div>
-    </div>
-</section>
+<?php include('Modules/app/Lib/appconf/appconf.php'); ?>
 
 <div class="ajax-loader"></div>
 
@@ -163,5 +147,8 @@ global $path, $session, $v;
     config.name = "<?php echo $name; ?>";
     config.public = <?php echo $public; ?>;
     config.db = <?php echo json_encode($config); ?>;
+
+    config.app_name = "<?php echo tr('Solar Template App'); ?>";
+    config.app_description = "<?php echo tr('A simpler version of the My Solar Battery app, power view only and key calculations'); ?>";
 </script>
 <script type="text/javascript" src="<?php echo $path; ?>Modules/app/apps/OpenEnergyMonitor/solartemplate/solartemplate.js?v=<?php echo time(); ?>"></script>
