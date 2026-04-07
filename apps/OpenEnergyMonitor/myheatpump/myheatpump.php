13cf4955b1be6e75d2fd8f6c95d151c11783129c
Trystan Lea <trystan.lea@gmail.com>
2026-04-07 11:43:36 +0100

    fix config

--- apps/OpenEnergyMonitor/myheatpump/myheatpump.php
+++ apps/OpenEnergyMonitor/myheatpump/myheatpump.php
@@ -2,11 +2,13 @@
 defined('EMONCMS_EXEC') or die('Restricted access');
 global $path, $session, $v;
 ?>
-<link href="<?php echo $path; ?>Modules/app/Views/css/config.css?v=<?php echo $v; ?>" rel="stylesheet">
 <link href="<?php echo $path; ?>Modules/app/Views/css/light.css?v=<?php echo $v; ?>" rel="stylesheet">
 
 <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Montserrat&amp;lang=en" />
-<script type="text/javascript" src="<?php echo $path; ?>Modules/app/Lib/appconf.js?v=<?php echo $v; ?>"></script>
+
+<link href="<?php echo $path; ?>Modules/app/Lib/appconf/appconf.css?v=<?php echo $v; ?>" rel="stylesheet">
+<script type="text/javascript" src="<?php echo $path; ?>Modules/app/Lib/appconf/appconf.js?v=<?php echo $v; ?>"></script>
+
 <script type="text/javascript" src="<?php echo $path; ?>Modules/feed/feed.js?v=102"></script>
 
 <script type="text/javascript" src="<?php echo $path; ?>Lib/flot/jquery.flot.min.js?v=<?php echo $v; ?>"></script>
@@ -392,7 +394,7 @@ global $path, $session, $v;
 
         </div>
       </div>
-      <div class="span5 app-config pt-3"></div>
+      <div id="app-config-content" class="span5 app-config pt-3"></div>
     </div>
   </div>
 </section>
